e250287a6af3276af147ecaefef2eeed065d213d
Mariia Sarbieieva <sarbeeva.m.a@gmail.com>
2023-08-11 15:39:05 +0300

    change basename

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter basename='/admin-panel-frontend'> 
+    <BrowserRouter > 
       <App />
       </ BrowserRouter>
       </Provider>
