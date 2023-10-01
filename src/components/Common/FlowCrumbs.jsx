9d40ccb5b5501e4c961efc5da49109e1f287d2d7
teiatan <protas.tb@gmail.com>
2023-10-01 03:15:40 +0300

    reafctor flow crumbs

--- src/components/Common/FlowCrumbs.jsx
+++ src/components/Common/FlowCrumbs.jsx
@@ -1,26 +1,27 @@
+import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
+import { Fragment } from 'react';
 import { Link } from "react-router-dom"
 
 export const FlowCrumbs = ({titles, redirections}) => {
     return (
         <div>
-            {titles.map((title, idx) => {
-                return (<>
+            {titles.map((title, idx) => 
+                <Fragment key={nanoid()}>
                     {redirections[idx]
                         ?
-                        <Link to={redirections[idx]}>{title}</Link>
+                        <Link to={redirections[idx]} key={nanoid()}>{title}</Link>
                         :
-                        <span>{title}</span>
+                        <span key={nanoid()}>{title}</span>
                     }
-
-                    {idx+1 < titles.length && <span> / </span>}
-                </>)
-            })}
+                    {idx+1 < titles.length && <span key={nanoid()}> / </span>}
+                </Fragment>                   
+            )}
         </div>
     )
 }
 
 FlowCrumbs.propTypes = {
-    titles: PropTypes.arrayOf(PropTypes.string),
+    titles: PropTypes.arrayOf(PropTypes.string).isRequired,
     redirections: PropTypes.arrayOf(PropTypes.string)
 };
