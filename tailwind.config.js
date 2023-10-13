3ed0cc336f3f7e15837b8a41bc91b7a961e962fc
teiatan <protas.tb@gmail.com>
2023-10-13 17:15:39 +0300

    fix: спроба 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,19 @@
-export * from "universal-components-frontend/tailwind.config"
+/** @type {import('tailwindcss').Config} */
+export default {
+  presets: [
+    // eslint-disable-next-line no-undef
+    require('./universal-components-frontend/tailwind.config')
+  ],
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  
+};
