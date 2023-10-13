8fc2c88ce7808130bd3a02f56d20b4b02c2eb70b
teiatan <protas.tb@gmail.com>
2023-10-13 15:24:16 +0300

    feat: підключено віддалений тейлвінд конфіг

--- src/components/Header/InfoHeaderSDevice.jsx
+++ src/components/Header/InfoHeaderSDevice.jsx
@@ -6,7 +6,7 @@ export const InfoHeaderSDevice = () => {
     return (
         <div className="flex justify-between items-center pr-[16px] pl-[16px] pt-[8px] pb-[4px]">
         <button type="button">
-          <MenuIcon className="w-[44px] h-[44px] stroke-iconWhite" />
+          <MenuIcon className="w-[44px] h-[44px] stroke-iconContrast" />
         </button>
         <a href="/">
           <LogoIcon color1="#fff" color2="#fff" size="44" />
