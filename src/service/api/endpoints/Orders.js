c5ff711eb149942e28b89ebd49eae20086756ec4
koseckaya <kosetsaya@gmail.com>
2023-12-04 18:03:34 +0200

    fix: rename request functions

--- src/service/api/endpoints/Orders.js
+++ src/service/api/endpoints/Orders.js
@@ -1,34 +1,37 @@
 import ApiClient from '../client'
 
-// get Order by Id
-const fetchOrderById = async (orderId) => {
+// Get Order Details by Id
+const getOrderDetails = async (orderId) => {
     const { data } = await ApiClient.get(`orders/${orderId}`);
     return data;
 };
 
 // get All Orders 
-const fetchOrders = async ({ page = 1, limit = 10, query = "" }) => {
+const getAllOrders = async ({ page = 1, limit = 10, query = "" }) => {
     const { data } = await ApiClient.get(
         `orders?page=${page}&limit=${limit}&query=${query}`
     );
     return data;
 };
 
-const addOrder = async (orderData) => {
-    const { data } = await ApiClient.post("orders/add", orderData);
+// Create Order by User
+const createOrderByUser = async (orderData) => {
+    const { data } = await ApiClient.post("orders", orderData);
     return data;
 };
 
+// Update Order
 const updateOrder = async (orderId, orderData) => {
-    const { data } = await ApiClient.patch(`orders/${orderId}`, orderData);
+    const { data } = await ApiClient.put(`orders/${orderId}`, orderData);
     return data;
 };
 
-const deleteOrderById = async (orderId) => {
+// Delete Order by User
+const deleteOrder = async (orderId) => {
     const { data } = await ApiClient.delete(`orders/${orderId}`);
     return data;
 };
 
 export default {
-    fetchOrderById, updateOrder, deleteOrderById, addOrder, fetchOrders
+    getOrderDetails, updateOrder, getAllOrders, createOrderByUser, deleteOrder
 }
