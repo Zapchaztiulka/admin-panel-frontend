26907c9b9163d45e9985ff0a7491d712877ef601
teitan <protas.tb@gmail.com>
2024-03-19 00:37:07 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: '/',
+  base: '/admin-panel-frontend/',
   resolve: {
     alias: {
       "@": fileURLToPath(new URL('./src', import.meta.url)),
